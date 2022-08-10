5a520010e4837b5f573ac687e92a59d16ca30995
kynuadien <qq2003@gmail.com>
2022-08-10 15:30:22 +0700

    testing - adi

--- application/views/dashboard/page.php
+++ application/views/dashboard/page.php
@@ -5,7 +5,7 @@
   </h1>
 </div>
 <!-- Content Row -->
-<div class="row">
+<div class="row ">
   <!-- Products Card -->
   <div class="col-xl-3 col-md-6 mb-4">
     <div class="card border-left-primary shadow h-100 py-2">
